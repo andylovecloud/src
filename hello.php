e44e61648834e75e1e9b8102befb10ae5a8d5d68
andylovecloud <andy.tranhuynh@gmail.com>
2024-01-26 10:58:33 +0200

    Initial commit

---
+++ hello.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Test</title>
+</head>
+<body>
+    <?php
+    //this is printing Hello to client
+    /* Multiple line comments
+
+    */
+    echo "<h1>Hello Andy!</h1>"
+    ?>    
+</body>
+</html>
+
